8810843c55b4c72cd2be6787c9c8ab1229955005
AnitaSalvas <130136433+AnitaSalvas@users.noreply.github.com>
2023-04-12 14:22:42 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,10 +1,13 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import ttest_1samp
+from scipy.stats import ttest_ind
 
 chat_id = 5072617748 # Ваш chat ID, не меняйте название переменной
 
-def solution(x: np.array) -> bool:
+def solution(x: np.array, y: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+    # Измените код этой функции
+    # Это будет вашим решением
+    # Не меняйте название функции и её аргументы
+    _, p_value = ttest_ind(x, y, equal_var=False)
     alpha = 0.03
-    t_stat, p_value = ttest_1samp(x, 300)
-    return p_value/2 < alpha and t_stat < 0
+    return p_value < alpha # Ваш ответ, True или False
