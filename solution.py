844801ee1b7873a3f70deb00efc6b2941c85ad07
AnitaSalvas <130136433+AnitaSalvas@users.noreply.github.com>
2023-04-12 11:51:29 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import ttest_1samp
 
+chat_id = 5072617748 # Ваш chat ID, не меняйте название переменной
 
-chat_id = 5072617748  # Ваш chat ID, не меняйте название переменной
-
-def solution(x, y) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+def solution(x: np.array) -> bool:
     alpha = 0.03
-    from scipy.stats import mannwhitneyu
-    p = mannwhitneyu(x, y, alternative='greater')[1]
-    return p <
+    t_stat, p_value = ttest_1samp(x, 300)
+    return p_value/2 < alpha and t_stat < 0
