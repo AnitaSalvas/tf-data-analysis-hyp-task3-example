9fbdf996993de2ef34809c0de69e43f38da4df26
AnitaSalvas <130136433+AnitaSalvas@users.noreply.github.com>
2023-04-12 14:42:03 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from scipy.stats import ttest_ind
+from scipy.stats import ztest_ind
 
 chat_id = 5072617748 # Ваш chat ID, не меняйте название переменной
 
@@ -8,6 +9,6 @@ def solution(x: np.array, y: np.array) -> bool: # Одна или две выб�
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    _, p_value = ttest_ind(x, y, equal_var=False)
+    _, p_value = ztest_ind(x, y, equal_var=False)
     alpha = 0.03
     return p_value < alpha # Ваш ответ, True или False
